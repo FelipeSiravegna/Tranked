dfc9b581b121978ca05784eff79b54e71d85d5b4
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-12-06 13:52:52 -0300

    Forgot password route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LogIn from './components/LogIn/LogIn';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {/* Component routes */}
         <Route path="/login" element={<LogIn/>} />
+        <Route path="/forgotpassword" element={<ForgotPassword/>} />
       </Routes>
     </Router>
   )
