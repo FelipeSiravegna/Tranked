5f2e8ace511ec1b85d8e1cbfabbcaac0d447750d
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-12-06 13:37:27 -0300

    Log in form css changes

--- src/components/MailPasswordForm/MailPasswordForm.jsx
+++ src/components/MailPasswordForm/MailPasswordForm.jsx
@@ -4,14 +4,14 @@ export default function MailPasswordForm(){
     return(
         <>
             <form class="flex justify-center">
-                <div class="flex flex-col w-1/4">
-                    <label for="mail"><b>Mail:</b></label>
-                    <input type="text" class="mb-5" placeholder="Enter mail"></input>
-                    <label for="password"><b>Password:</b></label>
-                    <input type="password" class="mb-5" placeholder="Enter password"></input>
-                    <p class="flex justify-center mb-5"><b>Forgot password?</b></p>
+                <div class="flex flex-col w-8/12">
+                    <label class="mt-24" for="mail"><b>Mail:</b></label>
+                    <input type="text" class="mb-5 mt-3 h-10" placeholder="Enter mail"></input>
+                    <label class="mt-6" for="password"><b>Password:</b></label>
+                    <input type="password" class="mb-5 mt-3 h-10" placeholder="Enter password"></input>
+                    <p class="flex justify-center mb-5 mt-6"><b>Forgot password?</b></p>
                 <div class="flex justify-center">
-                    <button type="submit" class="bg-violeta text-white w-1/2">Log in</button>
+                    <button type="submit" class="bg-violeta text-white w-1/2 mt-4 h-10 rounded-md">Log in</button>
                 </div>
                 </div>
             </form>
