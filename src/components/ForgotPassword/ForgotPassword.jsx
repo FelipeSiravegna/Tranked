c61ee52c7381caf28a0a24fd052e3b3737a40456
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-12-06 14:03:01 -0300

    Mail changed for Email

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -8,8 +8,8 @@ export default function ForgotPassword(){
             <form class="flex justify-center">
                 <div class="flex flex-col w-8/12">
                     <p class="mt-24">Enter your email below, and we'll send you a message with a link to reset your password</p>
-                    <label class="mt-12" for="mail"><b>Mail:</b></label>
-                    <input type="text" class="mb-5 mt-3 h-10" placeholder="Enter mail"></input>
+                    <label class="mt-12" for="mail"><b>Email:</b></label>
+                    <input type="text" class="mb-5 mt-3 h-10" placeholder="Enter email"></input>
                 <div class="flex justify-center">
                     <button type="submit" class="bg-violeta text-white w-1/2 mt-8 h-10 rounded-md">Send request</button>
                 </div>
--- src/components/MailPasswordForm/MailPasswordForm.jsx
+++ src/components/MailPasswordForm/MailPasswordForm.jsx
@@ -5,11 +5,11 @@ export default function MailPasswordForm(){
         <>
             <form class="flex justify-center">
                 <div class="flex flex-col w-8/12">
-                    <label class="mt-24" for="mail"><b>Mail:</b></label>
-                    <input type="text" class="mb-5 mt-3 h-10" placeholder="Enter mail"></input>
+                    <label class="mt-24" for="mail"><b>Email:</b></label>
+                    <input type="text" class="mb-5 mt-3 h-10" placeholder="Enter email"></input>
                     <label class="mt-6" for="password"><b>Password:</b></label>
                     <input type="password" class="mb-5 mt-3 h-10" placeholder="Enter password"></input>
-                    <p class="flex justify-center mb-5 mt-6"><b>Forgot password?</b></p>
+                    <p class="flex justify-center mb-5 mt-6"><b><a href="/forgotpassword">Forgot password?</a></b></p>
                 <div class="flex justify-center">
                     <button type="submit" class="bg-violeta text-white w-1/2 mt-4 h-10 rounded-md">Log in</button>
                 </div>
