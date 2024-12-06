550a3f76f9bd6db877ba8b48457e801980aa3c6c
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-12-06 14:08:36 -0300

    Sign up route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LogIn from './components/LogIn/LogIn';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         {/* Component routes */}
         <Route path="/login" element={<LogIn/>} />
         <Route path="/forgotpassword" element={<ForgotPassword/>} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </Router>
   )
