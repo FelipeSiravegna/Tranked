4f7a9a5431b574e97a56b30bedd70a6f5e12433e
Felipe Siravegna <felipesiravegna1@gmail.com>
2024-12-17 21:43:38 -0300

    Reset password route fixed, Profile route created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LogIn from './components/LogIn/LogIn';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import SignUp from './components/SignUp/SignUp';
 import ResetPassword from './components/ResetPassword/ResetPassword';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/login" element={<LogIn/>} />
         <Route path="/forgotpassword" element={<ForgotPassword/>} />
         <Route path="/signup" element={<SignUp/>} />
-        <Route path="resetpassword" element={<ResetPassword/>} />
+        <Route path="/resetpassword" element={<ResetPassword/>} />
+        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   )
