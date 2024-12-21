c8aef4d97dfd3c1a36c78cb486b6efcd8b661705
Felipe <felipesiravegna1@gmail.com>
2024-12-20 22:24:28 -0300

    Some routes now render the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import LogIn from './components/LogIn/LogIn';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import SignUp from './components/SignUp/SignUp';
 import ResetPassword from './components/ResetPassword/ResetPassword';
 import Profile from './components/Profile/Profile';
+import NavBar from './components/NavBar/NavBar';
+
+function LayoutWithNavBar(){
+  return(
+    <div>
+      <NavBar/>
+      {/* Componentes hijos */}
+      <Outlet/>
+    </div>
+  )
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Component routes */}
+        {/* Components with navbar */}
+        <Route element={<LayoutWithNavBar/>}>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
+
+        {/* Components without navbar */}
         <Route path="/login" element={<LogIn/>} />
         <Route path="/forgotpassword" element={<ForgotPassword/>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/resetpassword" element={<ResetPassword/>} />
-        <Route path="/profile" element={<Profile/>} />
       </Routes>
     </Router>
   )
